1ffe1999ccebaaecf38543f8b4b1dc730782e14d
OgheneMichael <zonalmeet@gmail.com>
2019-08-01 07:35:21 +0100

    Code for object literal improvements

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
-function multiply(a: number, b: number = 25) {
-	return a * b;
+const pizza = {
+	name: "Pepperoni",
+	price: 15,
+	getName() {
+		return this.name;
+	}
+};
+
+console.log(pizza.getName());
+
+const toppings = ["Pepperoni"];
+
+function createOrder(pizza: any, toppings: any) {
+	return { pizza, toppings };
 }
 
-console.log(multiply(5));
-console.log(multiply(5, 35));
+console.log(createOrder(pizza, toppings));
